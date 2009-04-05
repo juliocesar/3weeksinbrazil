0ca0d81c3e3fa91f11885daca8ca1000faff0849
Julio Cesar Ody <julioody@gmail.com>
2009-04-05 22:18:04 +1000

    so lets start on the code for a change

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'activerecord'
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter  => 'sqlite3',
+#   :dbfile   => 'naked.db'
+# )
 
 get '/' do
   haml :home, :layout => false
