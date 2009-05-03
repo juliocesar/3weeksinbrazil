78436f91fe497907329e62636a18c2f4a7884e98
Julio Cesar Ody <julioody@gmail.com>
2009-05-03 22:10:53 +1000

    3 turns to the left and one to the right

--- lib/photo.rb
+++ lib/photo.rb
@@ -2,5 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to              :post
   validates_presence_of   :image
   has_attached_file :image,
-    :path => "#{APP_ROOT}/public/photos/:id/:style/:basename.:extension"
+    :path => "#{APP_ROOT}/public/photos/:id/:style/:basename.:extension",
+    :url  => "/photos/:id/:style/:basename.:extension"
 end
