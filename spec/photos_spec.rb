cde7a1e5a0bf672c2409cdd947537548e9d57581
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2009-05-15 15:09:10 +1000

    moar specs

---
+++ spec/photos_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Photo do
+  describe "on #initialize" do
+    it "raises an exception if argument does not exist" do
+      lambda do
+        Photo.new 'odkpaksdoapsd'
+      end.should raise_error(Exception)
+    end
+    
+    it "raises an exception if argument is not an image" do
+      FileUtils.touch '/tmp/omg.txt'
+      lambda do
+        Photo.new '/tmp/omg.txt'
+      end.should raise_error(Exception)
+    end
+    
+    it "generates a polaroid on #build_polaroid"
+
+    it "skips generating a polaroid for a picture that already exists on #build_polaroid"
+    
+    it "places a symlink for both the original and the polaroid inside the public directory"
+    
+    it "removes all symlinks on #delete" 
+    
+    after do
+      FileUtils.rm_f '/tmp/omg.txt'
+    end
+  end
+end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -4,14 +4,35 @@ describe Post do
     
   before do
     FileUtils.mkdir_p POSTS_ROOT
-  end
-  
-  it "should return all valid posts in APP_ROOT/posts with #all" do
     FileUtils.mkdir POSTS_ROOT/'foo_post'
     FileUtils.touch POSTS_ROOT/'foo_post'/'text'
     FileUtils.touch POSTS_ROOT/'foo_post'/'body'
+  end
+  
+  it "returns all valid posts in APP_ROOT/posts with #all" do
     Post.all.should == ['foo_post']
   end
+  
+  it "returns an instance of Post on #open" do
+    Post.open('foo_post').should be_an_instance_of(Post)
+  end
+  
+  it "returns an array of Photos on #photos" do
+    FileUtils.mkdir POSTS_ROOT/'foo_post'/'photos'
+    FileUtils.touch POSTS_ROOT/'foo_post'/'photos'/'photo.jpg'
+    post = Post.open('foo_post')
+    post.photos.should_not be_empty # a lot less than ideal
+  end
+  
+  it "returns paginated results on Post#all"
+  
+  it "removes the post directory on #delete" # do
+  #     post = Post.open('foo_post')
+  #     post.delete!
+  #     File.directory?(POSTS_ROOT/'foo_post').should == false
+  #   end
+  
+  it "removes the symlink to the post's montage on #delete"
   
   after do
     FileUtils.rm_rf POSTS_ROOT
