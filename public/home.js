7fc94e2e97b2b42596122bca6f2653c5dbe7d134
Julio Cesar Ody <julioody@gmail.com>
2009-05-19 03:39:29 +0000

    ...

--- public/home.js
+++ public/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $.each(['pics', 'index', 'intro'], function(i, elt) {
+    $('<img />').attr('src', '/' + elt + '_underline.png');
     $('#' + elt + ' img').hover(
       function() { $(this).attr('src', '/' + elt + '_underline.png') },
       function() { $(this).attr('src', '/' + elt + '.png') }
