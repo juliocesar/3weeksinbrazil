c5492a1cf67d1606c4b6f99c3d66734c5cba5ea0
Julio Cesar Ody <julioody@gmail.com>
2009-05-19 03:33:17 +0000

    preloading images FOR REAL, fixed some silliness

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $.each(['next', 'previous'], function(i, elt) {
+    $('<img />').attr('src', '/' + '_underline.png');
     $('#' + elt + ' img').hover(
       function() { $(this).attr('src', '/' + elt + '_underline.png') },
       function() { $(this).attr('src', '/' + elt + '.png') }
